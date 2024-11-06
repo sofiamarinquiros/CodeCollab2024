d73c61541380880c104f4d01364a4fd5d048c6a2
junworkhong <junworkhong@gmail.com>
2024-11-06 10:33:54 -0500

    Update firebaseAdmin.js

--- dating-app-backend/firebaseAdmin.js
+++ dating-app-backend/firebaseAdmin.js
@@ -1,8 +1,11 @@
+//dotenv is for service account key (ignored)
+require("dotenv").config();
+
 //Importing Firebase Admin SDK
 const admin = require("firebase-admin");
 
-//Import service account key
-const serviceAccount = require("./firebaseServiceAccountKey.json");
+//Import service account key from dotenv
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 //Initialize Firebase Admin with service account credentials
 admin.initializeApp({
